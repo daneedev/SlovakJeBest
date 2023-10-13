c8f3b44d9ed772720b5029db87ff69c4d85444cd
daneeskripter <imdanee@seznam.cz>
2023-10-13 08:57:50 +0200

    rate limit

--- index.js
+++ index.js
@@ -2,27 +2,38 @@ const express = require('express');
 const app = express();
 const fs = require("fs")
 let data = JSON.parse(fs.readFileSync(__dirname + "/data.json"));
+const { rateLimit } = require("express-rate-limit")
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    limit: 10
+})
+
+const clickerLimiter = rateLimit({
+    windowMs: 50,
+    limit: 1
+})
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function (req, res) {
+app.get("/", limiter, function (req, res) {
     data = JSON.parse(fs.readFileSync(__dirname + "/data.json"));
     data.visitCount = data.visitCount + 1
     fs.writeFileSync(__dirname + "/data.json", JSON.stringify(data)); 
     res.render(__dirname + "/views/index.ejs", { visitcount: data.visitCount})
 })
 
-app.get("/info", function (req, res) {
+app.get("/info", limiter, function (req, res) {
     res.render(__dirname + "/views/info.ejs")
 })
 
-app.get("/clicker", function (req, res) {
+app.get("/clicker", limiter, function (req, res) {
     res.render(__dirname + "/views/clicker.ejs", { clicks: data.clicks})
 })
 
-app.post('/click', (req, res) => {
+app.post('/click', clickerLimiter, (req, res) => {
     data = JSON.parse(fs.readFileSync(__dirname + "/data.json"));
     data.clicks++;
     fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
